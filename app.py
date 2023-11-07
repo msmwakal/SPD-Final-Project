30bea36cba79118d8a2600649afb80ae2ffc1859
msmwakal <141044464+msmwakal@users.noreply.github.com>
2023-11-07 09:43:42 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask #The FLASK framework (the webserver)
+from flask import render_template  # For opening and read the HTML file and showing them as the webpage
+from flask import request  #Allows me to get stuff from the URL (the ?)
+
+from flask import redirect, url_for, g # Use images from the static folder
+#from projects import Project
+
+
+app=Flask(__name__)
+# Change DATABASE
+#app.config['DATABASE'] = 'database.db'
+#db = Project('database.db')
+#db.create_table()
+
+@app.route("/")  
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+@app.route("/listings") 
+def listings():
+    return render_template('listings.html')
+
+@app.route("/reviews") 
+def reviews():
+    return render_template('reviews.html')
+
+@app.route("/inbox") 
+def inbox():
+    return render_template('inbox.html')
+
+# for some forsaken reason this is not working even though it is identical to the other ones
+@app.route("/login") 
+def login():
+    return render_template('login.html')
+
+app.run(debug=True)
